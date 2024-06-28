b1d2eab1dd6fb897f46d2d0d34fc2773be31041b
harunsakin1 <harunsakin16@gmail.com>
2024-06-28 14:20:22 +0300

    deneme 2

--- src/main/java/com/haruns/Main.java
+++ src/main/java/com/haruns/Main.java
@@ -6,29 +6,30 @@ public class Main {
     public static void main(String[] args)
     {
 
-//        System.out.println("Hello world! Barış");
-//        /*char karakter;
-//        karakter=146;
-//        System.out.println(karakter);*/
-//        int sayi1,sayi2,toplam;
-//        Scanner sc=new Scanner(System.in);
-//        System.out.println("1.Sayiyi giriniz");
-//        sayi1=sc.nextInt();
-//
-//        System.out.println("2.Sayiyi giriniz");
-//        sayi2=sc.nextInt();
-//
-//        toplam=sayi1+sayi2;
-//        System.out.println("Toplam : "+toplam);
-//
-//        int s1,s2,carpim;
-//       // Scanner scanner=new Scanner(System.in);
-//        System.out.println("1.Sayi : ");
-//        s1=sc.nextInt();
-//        System.out.println("2.Sayi : ");
-//        s2=sc.nextInt();
-//        carpim=s1*s2;
-//        System.out.println("Carpim : "+carpim);
+        System.out.println("Hello world! Barış");
+        /*char karakter;
+        karakter=146;
+        System.out.println(karakter);*/
+        int sayi1,sayi2,toplam;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("1.Sayiyi giriniz");
+        sayi1=sc.nextInt();
+
+        System.out.println("2.Sayiyi giriniz");
+        sayi2=sc.nextInt();
+
+        toplam=sayi1+sayi2;
+        System.out.println("Toplam : "+toplam);
+
+        int s1,s2,carpim;
+       // Scanner scanner=new Scanner(System.in);
+        System.out.println("1.Sayi : ");
+        s1=sc.nextInt();
+        System.out.println("2.Sayi : ");
+        s2=sc.nextInt();
+        carpim=s1*s2;
+        System.out.println("Carpim : "+carpim);
+        System.out.println("Git 2. Pushlama");
     }
 
 
