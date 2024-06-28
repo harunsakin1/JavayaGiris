59d7134825df43bf80a0027ff1fbb64f8b3881b2
harunsakin1 <harunsakin16@gmail.com>
2024-06-28 13:53:28 +0300

    Initial commit

---
+++ src/main/java/com/haruns/TipDonusumleriOrnek.java
@@ -0,0 +1,19 @@
+package com.haruns;
+
+public class TipDonusumleriOrnek {
+    public static void main(String[] args) {
+        byte b;
+        int i=257;
+        double d=323.142;
+        System.out.println("int >> byte dönüşümü");
+        b=(byte) i;
+        System.out.println("i= "+i+" b = "+ b);
+        System.out.println("double >> int dönüşümü");
+        i=(int) d;
+        System.out.println("d = "+d+" b = "+i);
+        System.out.println("double >> byte dönüşümü");
+        b=(byte) d;
+        System.out.println("d = "+d+" b = "+b);
+        double sqrt = Math.sqrt((5 * 5 - 4 * 4);
+    }
+}
